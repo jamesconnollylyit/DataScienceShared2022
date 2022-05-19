ee0399202baa07f60e74de3a2e564bc5db06cc67
James Connolly <james.connolly@lyit.ie>
2022-05-19 10:01:29 +0100

    Create Insurance session 1.R

---
+++ Insurance session 1.R
@@ -0,0 +1,64 @@
+# Insurance dataset 
+# Load the dataset into a data frame first
+
+insurance_data <- read.csv("insurance.csv", na = "")
+str(insurance_data)
+
+# several variables need to be converted
+# sex - male = 0, female = 1
+# Smoker - yes = 1, no = 0
+# Region contains 4 categories
+# N = 4, so we need n-1 indicator variables
+# = 3 indicator variables
+# Code variables in alphabetical order
+head(insurance_data$region, 15)
+
+# Convert variables as described above
+names(insurance_data)
+attach(insurance_data)
+
+insurance_data$sex <- factor(sex,
+                             levels = c("male", "female"), 
+                             ordered = FALSE)
+
+insurance_data$smoker <- factor(smoker, 
+                                levels = c("yes", "no"), 
+                                ordered = FALSE)
+
+insurance_data$region <- factor(region,  
+                             levels = c("northeast", "northwest", "southeast", "southwest"), 
+                             ordered = FALSE)
+
+str(insurance_data)
+
+# View the split of males and females within the data frame
+table(insurance_data$sex)
+table(insurance_data$smoker)
+table(insurance_data$region)
+
+pairs(insurance_data)
+# Initial investigation of data variables
+install.packages("psych")
+library(psych)
+
+# Seems there could be a positive correlation between 
+# smoker and charges, perhaps charges and age
+# and BMI and charges
+pairs.panels(insurance_data,
+             smooth = FALSE,      # If TRUE, draws loess smooths
+             scale = FALSE,      # If TRUE, scales the correlation text font
+             density = TRUE,     # If TRUE, adds density plots and histograms
+             ellipses = FALSE,    # If TRUE, draws ellipses
+             method = "pearson",# Correlation method (also "pearson" or "kendall")
+             pch = 21,           # pch symbol
+             lm = FALSE,         # If TRUE, plots linear fit rather than the LOESS (smoothed) fit
+             cor = TRUE,         # If TRUE, reports correlations
+             jiggle = FALSE,     # If TRUE, data points are jittered
+             factor = 2,         # Jittering factor
+             hist.col = 4,       # Histograms color
+             stars = TRUE,       # If TRUE, adds significance level with stars
+             ci = TRUE)          # If TRUE, adds confidence intervals
+
+# if we build the model now, R will automatically split the
+# factor variables
+# Alternatively we will control this process
